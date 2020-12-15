8df3c0c2c0b2fee2a048105ffabe153ab018e43c
advoet <advoet@gmail.com>
2020-12-15 14:20:25 -0800

    integrating user review query into flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,20 @@
+# FLASK APP
 from flask import Flask, render_template, request
 from wtforms import Form, TextAreaField, validators
 from wtforms.validators import ValidationError
+from flask_table import Table, Col
 
+# MODEL
 import pickle
 import os
 import numpy as np
 
+# WEB
 import requests
 from bs4 import BeautifulSoup
+
+# UTIL
+from utils import user_review_scraper
 
 #### PREPARING THE MODEL
 
@@ -16,12 +23,9 @@ MODEL_PATH = ABS_PATH + 'pkl_objects/'
 MODEL = MODEL_PATH + 'simple_svd.pkl'
 model = pickle.load(open(MODEL, 'rb'))
 
-
-
 #### RECOMMENDATIONS
 
 recommend_from = None
-BGG_ERROR_MESSAGE = 'Error: User does not exist'
 
 
 #### FLASK APP
@@ -32,21 +36,42 @@ class UserForm(Form):
 	username = TextAreaField('BGG username:',[validators.DataRequired()])
 
 	def validate_username(form, field):
-		page = requests.get('https://boardgamegeek.com/user/{username}')
+		page = requests.get(f'https://boardgamegeek.com/user/{field.data}')
 		if page.text.find('User does not exist') > -1:
 			raise ValidationError('User does not exist')
+
+class ReviewTable(Table):
+    bgg_id = Col('Game ID')
+    game = Col('Game')
+    rating = Col('Rating')
+class Item(object):
+	def __init__(self, game, bgg_id, rating):
+		self.game = game
+		self.bgg_id = bgg_id
+		self.rating = rating
 
 @app.route('/')
 def index():
 	form = UserForm(request.form)
 	return render_template('first_app.html', form = form)
 
-@app.route('/hello', methods = ['POST'])
-def hello():
+@app.route('/reviews', methods = ['POST'])
+def get_reviews():
 	form = UserForm(request.form)
 	if request.method == 'POST' and form.validate():
-		name = request.form['sayhello']
-		return render_template('hello.html', name = name)
+		name = request.form['username']
+		reviews = user_review_scraper.get_review_list(name)
+		reviews = [Item(*review) for review in reviews]
+		reviews = ReviewTable(reviews)
+		return render_template('reviews.html', name = name, reviews = reviews)
+	return render_template('first_app.html', form = form)
+
+@app.route('/recommendations', methods = ['POST'])
+def get_recommendations():
+	form = UserForm(request.form)
+	if request.method == 'POST' and form.validate():
+		name = request.form['username']
+		return render_template('recommendations.html', name = name)
 	return render_template('first_app.html', form = form)
 
 if __name__ == '__main__':
